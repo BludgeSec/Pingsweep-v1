c87137106cb044b918cf274936a8b95c5ebba438
1 <1>
2024-11-08 10:03:12 +0000

    1

--- Pingsweep-v1/Form1.Designer.cs
+++ Pingsweep-v1/Form1.Designer.cs
@@ -28,11 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-
             this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // listView1
             // listView1
             // 
             this.listView1.HideSelection = false;
@@ -42,21 +41,32 @@
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(829, 817);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Pingsweep-v1/Form1.cs
+++ Pingsweep-v1/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Pingsweep_v1
@@ -16,6 +12,10 @@ namespace Pingsweep_v1
         {
             InitializeComponent();
             InitializeListView(); // Initialize ListView columns
+
+            // Get the local IP and display it in the Label
+            string localIP = GetLocalIP();
+            textBox1.Text = "Local IP Address: " + localIP; // Assuming 'labelLocalIP' is the name of the Label control
         }
 
         // Create a ListViewItem with IP address, hostname, ping response time, and shared folders count
@@ -24,8 +24,6 @@ namespace Pingsweep_v1
             ListViewItem item = new ListViewItem(ip);
             item.Tag = ip; // Store IP address as tag for context menu
             item.SubItems.Add(hostName);
-
-
             return item;
         }
 
@@ -35,9 +33,40 @@ namespace Pingsweep_v1
             listView1.View = View.Details;
             listView1.Columns.Add("IP Address", 100);
             listView1.Columns.Add("Hostname", 180);
-
         }
 
+        // Get the local IP address of the machine
+        private string GetLocalIP()
+        {
+            string localIP = string.Empty;
+            foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in networkInterface.GetIPProperties().UnicastAddresses)
+                    {
+                        // Check for IPv4 addresses (ignore IPv6)
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            localIP = ip.Address.ToString();
+                            break;
+                        }
+                    }
+                }
+                if (!string.IsNullOrEmpty(localIP))
+                    break;
+            }
+            return localIP;
+        }
+        // Define the event handler method
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Add your logic here
+            // For example, if you want to update a label or perform an action when the text changes in the TextBox
+            string text = textBox1.Text; // Get the text from textBox1
+                                         // Perform some action with the text, like filtering a list, etc.
+            Console.WriteLine("Text changed: " + text);
+        }
 
     }
 }
