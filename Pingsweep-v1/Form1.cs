9e3bc8eb103d429c7e667c0d47e3c75a28fb5e4d
chrishill <chrishill@IOT24>
2024-11-08 09:39:22 +0000

    Add project files.

---
+++ Pingsweep-v1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pingsweep_v1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeListView(); // Initialize ListView columns
+        }
+
+        // Create a ListViewItem with IP address, hostname, ping response time, and shared folders count
+        private ListViewItem CreateListViewItem(string ip, string hostName, string osInfo, long pingTime, int sharedFoldersCount, int[] openPorts)
+        {
+            ListViewItem item = new ListViewItem(ip);
+            item.Tag = ip; // Store IP address as tag for context menu
+            item.SubItems.Add(hostName);
+
+
+            return item;
+        }
+
+        // Initialize ListView columns
+        private void InitializeListView()
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("IP Address", 100);
+            listView1.Columns.Add("Hostname", 180);
+
+        }
+
+
+    }
+}
